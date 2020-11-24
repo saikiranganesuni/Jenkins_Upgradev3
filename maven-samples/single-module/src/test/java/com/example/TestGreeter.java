287b5f59db0c481a832baf35d174ad2c042e5e7e
sai kiran <saikiranganesuni@gmail.com>
2020-11-23 22:09:53 -0800

    fjes

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+   @Test
+  public void greetShouldIncludeGreetingMessage1() {
+    String someone = "World, Thanks you making me Happy123";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
